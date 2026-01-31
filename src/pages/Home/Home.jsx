8b7232f10fcf5b5b11b40ef0cb7cd0fd6c60ce0d
Jsoza1 <jsoza993@gmail.com>
2026-01-30 23:44:48 -0300

    feat: add Home page component with hero, history, services, and call-to-action sections.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
                     rel="noopener noreferrer"
                     className="btn btn-primary hero-btn"
                 >
-                    Reservar Mesa
+                    Reservar &nbsp; Mesa
                 </a>
             </section>
 
